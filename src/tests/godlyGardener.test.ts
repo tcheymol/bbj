6e4015af43054b076293be6647b4e869d30805c2
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2025-04-24 18:25:32 +0200

    :test_tube: [GodlyGardener] Add first mowing test

---
+++ src/tests/godlyGardener.test.ts
@@ -0,0 +1,12 @@
+import { TheGodlyGardener } from "../Domain/Mowing/TheGodlyGardener";
+
+describe('The Godly Gardener basic tests', () => {
+    it('should handle the land', () => {
+        const gardener = new TheGodlyGardener(['55', '00 N', 'F']);
+
+        const land = gardener.getLand();
+        const mower = land.mowers[0];
+
+        expect(mower.getPosition()).toBe('01 N');
+    });
+});
